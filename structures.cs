87fec2afe6d152f282adca108113b8f984237c7c
Mitch <warframeia1501@gmail.com>
2022-11-30 14:13:17 -0700

    Inital commit, Fully working

---
+++ structures.cs
@@ -0,0 +1,78 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+namespace UI_Mimic
+{
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct MouseInput
+    {
+        internal int dx;
+        internal int dy;
+        internal uint mouseData;
+        internal uint dwFlags;
+        internal uint time;
+        internal IntPtr dwExtraInfo;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct KeyboardInput
+    {
+        internal ushort wVk;
+        internal ushort wScan;
+        internal uint dwFlags;
+        internal uint time;
+        internal IntPtr dwExtraInfo;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct HardwareInput
+    {
+        internal uint uMsg;
+        internal ushort wParamL;
+        internal ushort wParamH;
+    }
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct InputUnion
+    {
+        [FieldOffset(0)] internal MouseInput mi;
+        [FieldOffset(0)] internal KeyboardInput ki;
+        [FieldOffset(0)] internal HardwareInput hi;
+    }
+    internal struct Input
+    {
+        internal int type;
+        internal InputUnion u;
+    }
+    [Flags]
+    internal enum InputType
+    {
+        Mouse = 0,
+        Keyboard = 1,
+        Hardware = 2
+    }
+    [Flags]
+    internal enum KeyEventF
+    {
+        KeyDown = 0x0000,
+        ExtendedKey = 0x0001,
+        KeyUp = 0x0002,
+        Unicode = 0x0004,
+        Scancode = 0x0008
+    }
+    [Flags]
+    internal enum MouseEventF
+    {
+        Absolute = 0x8000,
+        HWheel = 0x01000,
+        Move = 0x0001,
+        MoveNoCoalesce = 0x2000,
+        LeftDown = 0x0002,
+        LeftUp = 0x0004,
+        RightDown = 0x0008,
+        RightUp = 0x0010,
+        MiddleDown = 0x0020,
+        MiddleUp = 0x0040,
+        VirtualDesk = 0x4000,
+        Wheel = 0x0800,
+        XDown = 0x0080,
+        XUp = 0x0100
+    }
+}
